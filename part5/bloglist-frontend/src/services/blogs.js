4f3ba6d30410e5f71f097868d3ce106cf8fb057a
sonkkeli <46299415+sonkkeli@users.noreply.github.com>
2019-09-01 21:01:13 +0300

    Done until 5.10*

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -12,9 +12,14 @@ const getAll = () => {
   return req.then(res => res.data)
 }
 
+const getOne = (id) => {
+  const req = axios.get(`${baseUrl}/${id}`)
+  return req.then(res => res.data)
+}
+
 const create = async newObject => {
   const config = {
-    headers: { Authorization: token },
+    headers: { Authorization: token }
   }
 
   const response = await axios.post(baseUrl, newObject, config)
@@ -22,8 +27,19 @@ const create = async newObject => {
 }
 
 const update = (id, newObject) => {
-  const request = axios.put(`${ baseUrl } /${id}`, newObject)
+  const config = {
+    headers: { Authorization: token }
+  }
+  const request = axios.put(`${baseUrl}/${id}`, newObject, config)
   return request.then(response => response.data)
 }
 
-export default { getAll, create, update, setToken }
+const remove = (id) => {
+  const config = {
+    headers: { Authorization: token }
+  }
+  const request = axios.delete(`${baseUrl}/${id}`, config)
+  return request.data
+}
+
+export default { getAll, getOne, create, update, remove, setToken }
