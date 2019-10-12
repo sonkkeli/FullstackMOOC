976915ea64945a6314189c723821a74966bb269a
sonkkeli <laurilasonja@gmail.com>
2019-10-12 22:36:55 +0300

    6.16 done

--- part6/redux-anecdotes-master/src/App.js
+++ part6/redux-anecdotes-master/src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
+import anService from './services/anecdotes'
+import {initialize} from './reducers/anecdoteReducer'
 
-const App = () => {
+const App = (props) => {
+
+  useEffect(() => {
+    anService.getAll().then(a => props.initialize(a))
+  })
 
   return (
     <div>
@@ -17,4 +24,4 @@ const App = () => {
   )
 }
 
-export default App
+export default connect(null, { initialize })(App)
--- part6/redux-anecdotes-master/src/index.js
+++ part6/redux-anecdotes-master/src/index.js
@@ -14,7 +14,6 @@ const reducer = combineReducers({
 })
 
 const store = createStore(reducer)
-console.log(store.getState())
 
 const render = () => {
   ReactDOM.render(
