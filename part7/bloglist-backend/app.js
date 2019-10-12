f4f3960e2f54b69c040fa32a091710b23a015a32
sonkkeli <laurilasonja@gmail.com>
2019-10-12 18:26:24 +0300

    7.11 backend part done

---
+++ part7/bloglist-backend/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
+const cors = require('cors')
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/api', (req, res) => {
+  res.send('<p>continue to /api/blogs to see what you are looking for</p>')
+})
+
+app.use(middleware.tokenExtractor)
+
+app.use('/api/users', usersRouter)
+app.use('/api/blogs', blogsRouter)
+app.use('/api/login', loginRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
