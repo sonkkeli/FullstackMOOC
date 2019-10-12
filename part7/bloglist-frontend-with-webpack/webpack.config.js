bb1f8a96fe0b694fcb698f8640b413447f749122
sonkkeli <laurilasonja@gmail.com>
2019-10-12 20:07:36 +0300

    7.16 done

---
+++ part7/bloglist-frontend-with-webpack/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+const config = (env, argv) => {
+  console.log('argv', argv.mode)
+
+  const backend_url = argv.mode === 'production'
+    ? 'https://TODOCHANGE.herokuapp.com/'
+    : 'http://localhost:3003/'
+
+  return {
+    entry: ['@babel/polyfill', './src/index.js'],
+    output: {
+      path: path.resolve(__dirname, './build'),
+      filename: 'main.js',
+      publicPath: '/'
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, 'build'),
+      compress: true,
+      port: 3000,
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
+        },
+        {
+          test: /\.css$/,
+          loaders: ['style-loader', 'css-loader'],
+        },
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        BACKEND_URL: JSON.stringify(backend_url)
+      }),
+      new HtmlWebPackPlugin({
+        template: 'public/index.html',
+        filename: 'index.html'
+      })
+    ]
+  }
+}
+
+module.exports = config
