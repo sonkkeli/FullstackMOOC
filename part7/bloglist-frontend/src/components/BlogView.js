86404f27da1fa81de7fc1242a6c4d4c11cc7ce52
sonkkeli <laurilasonja@gmail.com>
2019-10-12 18:02:05 +0300

    7.7-7.10 done

---
+++ part7/bloglist-frontend/src/components/BlogView.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const BlogView = ({ signedInUser, blog, addLike }) => {
+  if ( blog === undefined) { 
+    return <p>Loading</p>
+  }
+
+  if (signedInUser === null){
+    return null
+  }
+
+  return (
+    <div key={blog.id} className="container">
+      <br/>
+      <h2>{blog.title} by <span style={{fontStyle: 'italic'}}>{blog.author}</span></h2>
+      <br/>
+      <a href={blog.url}>{blog.url}</a>
+      <br/><br/>
+      <p>
+        {blog.likes} likes 
+        <button className="ml-3 btn btn-dark" data-testid={`likebtn`} onClick={addLike} value={JSON.stringify(blog)}>
+          {'<3 like'}
+        </button>
+      </p>
+      <p>added by {blog.user.name}</p>
+    </div>
+  )
+}
+
+BlogView.propTypes = {
+  blog: PropTypes.object
+}
+
+export default BlogView
