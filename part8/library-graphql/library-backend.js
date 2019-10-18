436bd745a87edd84797ac33056c28ef6b3cd63ed
sonkkeli <laurilasonja@gmail.com>
2019-10-18 21:17:18 +0300

    8.7 done

--- part8/library-graphql/library-backend.js
+++ part8/library-graphql/library-backend.js
@@ -19,6 +19,10 @@ const typeDefs = gql`
       author: String!
       genres: [String!]!
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 
   type Author {
@@ -68,6 +72,13 @@ const resolvers = {
         authors = authors.concat(author)
       } 
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+      if (!author) return null
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+      return updatedAuthor
     }
   },
 
